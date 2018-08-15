04519235b88ce1ea27fe2afbafe919a2405838b0
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-16 00:47:48 +0200

    Removed servlet from application

--- src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutPanelContainer.java
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutPanelContainer.java
@@ -1,7 +1,6 @@
 package com.rieger.gwt.tutorials.layoutpanel.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.LayoutPanel;
@@ -12,18 +11,6 @@ import com.google.gwt.user.client.ui.Widget;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class LayoutPanelContainer implements EntryPoint {
-    /**
-     * The message displayed to the user when the server cannot be reached or
-     * returns an error.
-     */
-    private static final String SERVER_ERROR = "An error occurred while "
-	    + "attempting to contact the server. Please check your network " + "connection and try again.";
-
-    /**
-     * Create a remote service proxy to talk to the server-side Greeting service.
-     */
-    private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
     /**
      * This is the entry point method.
      */
