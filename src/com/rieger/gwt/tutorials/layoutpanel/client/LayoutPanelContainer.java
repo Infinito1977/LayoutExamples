b6c97d82d3b4cc1b26e7ef34e4e4a3e53bd6d05e
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-16 00:49:01 +0200

    Renamed child0-2 to leftPanel, rightPanel, centerPanel

--- src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutPanelContainer.java
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutPanelContainer.java
@@ -15,21 +15,21 @@ public class LayoutPanelContainer implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-	Widget child0 = new HTML("Left Panel"), child1 = new HTML("Right Panel"), child2 = new HTML("Center Panel");
+	Widget leftPanel = new HTML("Left Panel"), rightPanel = new HTML("Right Panel"), centerPanel = new HTML("Center Panel");
 	LayoutPanel p = new LayoutPanel();
-	child0.getElement().setClassName("panelWithBorder");
-	child1.getElement().setClassName("panelWithBorder");
-	child2.getElement().setId("centerPanel");
-	p.add(child0);
-	p.add(child1);
-	p.add(child2);
+	leftPanel.getElement().setClassName("panelWithBorder");
+	rightPanel.getElement().setClassName("panelWithBorder");
+	centerPanel.getElement().setId("centerPanel");
+	p.add(leftPanel);
+	p.add(rightPanel);
+	p.add(centerPanel);
 
-	p.setWidgetLeftWidth(child0, 0, Unit.PCT, 50, Unit.PCT); // Left panel 50% (PCT) from left
-	p.setWidgetRightWidth(child1, 0, Unit.PCT, 50, Unit.PCT); // Right panel 50% (PCT) from right
+	p.setWidgetLeftWidth(leftPanel, 0, Unit.PCT, 50, Unit.PCT); // Left panel 50% (PCT) from left
+	p.setWidgetRightWidth(rightPanel, 0, Unit.PCT, 50, Unit.PCT); // Right panel 50% (PCT) from right
 
 	// Center Panel
-	p.setWidgetLeftRight(child2, 5, Unit.EM, 5, Unit.EM); // 5em left and right border
-	p.setWidgetTopBottom(child2, 5, Unit.EM, 5, Unit.EM); // 5em top and bottom border
+	p.setWidgetLeftRight(centerPanel, 5, Unit.EM, 5, Unit.EM); // 5em left and right border
+	p.setWidgetTopBottom(centerPanel, 5, Unit.EM, 5, Unit.EM); // 5em top and bottom border
 
 	RootLayoutPanel.get().add(p);
     }
