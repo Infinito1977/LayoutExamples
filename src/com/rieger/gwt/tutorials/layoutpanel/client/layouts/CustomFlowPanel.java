0e1b2121f10ec7fd4fe47ee775c8bfa2bf7c8363
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-20 18:02:17 +0200

    Removed DockLayoutPanel from CustomFlowPanel

--- src/com/rieger/gwt/tutorials/layoutpanel/client/layouts/CustomFlowPanel.java
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/layouts/CustomFlowPanel.java
@@ -1,27 +1,22 @@
 package com.rieger.gwt.tutorials.layoutpanel.client.layouts;
 
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 
-public class CustomFlowPanel extends DockLayoutPanel {
+public class CustomFlowPanel extends FlowPanel {
     public CustomFlowPanel() {
-	super(Unit.EM);
 	Widget north = new HTML(
 		"<h1>Elemente sind auf 10% Breite begrenzt, sonst füllen sie die komplette Spalte</h1>");
-	addNorth(north, 4);
-	FlowPanel p = new FlowPanel();
+	add(north);
 	for (int i = 0; i < 9; i++) {
-	    Widget w = new HTML("<h2>" + i + "</h2>");
+	    Widget w = new HTML("<h2>" + (i + 1) + "</h2>");
 	    w.setStyleName("border border-primary rounded gwt-width-10pc");
-	    p.add(w);
+	    add(w);
 	}
 	// Fügt vor 9. Element folgendes Element hinzu
 	Widget w_a = new HTML("<h2>a</h2>");
 	w_a.setStyleName("border border-primary rounded gwt-width-10pc");
-	p.insert(w_a, 8);
-	add(p);
+	insert(w_a, 8);
     }
 }
