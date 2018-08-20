4176379c58ce2f01ac206ab2e8d6eef6e682706d
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-20 15:29:57 +0200

    Select starting tab via CustomConstants.properties

---
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/i8n/CustomConstants.java
@@ -0,0 +1,8 @@
+package com.rieger.gwt.tutorials.layoutpanel.client.i8n;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface CustomConstants extends Constants {
+    @DefaultIntValue(0)
+    int selectedTab();
+}
--- src/com/rieger/gwt/tutorials/layoutpanel/client/layouts/CustomTabLayoutPanel.java
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/layouts/CustomTabLayoutPanel.java
@@ -1,12 +1,16 @@
 package com.rieger.gwt.tutorials.layoutpanel.client.layouts;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.rieger.gwt.tutorials.layoutpanel.client.i8n.CustomConstants;
 
 public class CustomTabLayoutPanel extends DockLayoutPanel {
+    private CustomConstants constants = GWT.create(CustomConstants.class);
+    
     public CustomTabLayoutPanel() {
 	super(Unit.EM);
 	Widget north = new HTML("<h1>TabLayoutPanel</h1>");
@@ -24,6 +28,8 @@ public class CustomTabLayoutPanel extends DockLayoutPanel {
 	p.add(new CustomFlexTable(), "FlexTable");
 	p.add(new CustomGrid(), "Grid inside DecoratorPanel");
 	p.add(new CustomDeckPanel(), "DeckPanel inside VerticalPanel");
+	
+	p.selectTab(constants.selectedTab());
 
 	add(p);
 	CustomCssFormatter.addGWTStyles(this);
