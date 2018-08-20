29e3f0e1e0da5d495faffce4ed47a54612c512fd
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-20 14:51:08 +0200

    Lore ipsum is now supplied from CustomMessages

---
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/i8n/CustomMessages.java
@@ -0,0 +1,8 @@
+package com.rieger.gwt.tutorials.layoutpanel.client.i8n;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface CustomMessages extends Messages {
+    @DefaultMessage("Test")
+    String loreIpsum10k();
+}
