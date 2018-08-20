865632f4f84b7dad81df927a20fd6256d438907d
Marco Rieger <Marco.Rieger@gmx.de>
2018-08-20 15:05:55 +0200

    Refactor: moved HorizontalPanel code to CustomHorizontalPanel

--- src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutExamples.java
+++ src/com/rieger/gwt/tutorials/layoutpanel/client/LayoutExamples.java
@@ -6,7 +6,6 @@ import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
@@ -16,6 +15,7 @@ import com.google.gwt.user.client.ui.Widget;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomDockLayoutPanel;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomFlexTable;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomFlowPanel;
+import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomHorizontalPanel;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomLayoutPanel;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomCssFormatter;
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomDeckPanel;
@@ -23,20 +23,6 @@ import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomSplitLayoutPane
 import com.rieger.gwt.tutorials.layoutpanel.client.layouts.CustomStackLayoutPanel;
 
 public class LayoutExamples implements EntryPoint {
-    public HorizontalPanel getHorizontalPanel() {
-	HorizontalPanel p = new HorizontalPanel();
-	for (int i = 0; i < 9; i++) {
-	    Widget w = new HTML("<h2>" + i + "</h2>");
-	    w.setStyleName("border border-primary rounded");
-	    p.add(w);
-	}
-	// Fügt vor 9. Element folgendes Element hinzu
-	Widget w_a = new HTML("<h2>a</h2>");
-	w_a.setStyleName("border border-primary rounded");
-	p.insert(w_a, 8);
-	return p;
-    }
-
     public VerticalPanel getVerticalPanel() {
 	VerticalPanel p = new VerticalPanel();
 	for (int i = 0; i < 9; i++) {
@@ -81,7 +67,7 @@ public class LayoutExamples implements EntryPoint {
 	p.add(new CustomSplitLayoutPanel(), "SplitLayoutPanel");
 	p.add(new CustomStackLayoutPanel(Unit.EM), "StackLayoutPanel");
 	p.add(new CustomFlowPanel(), "FlowPanel");
-	p.add(getHorizontalPanel(), "HorizontalPanel");
+	p.add(new CustomHorizontalPanel(), "HorizontalPanel");
 	p.add(getVerticalPanel(), "VerticalPanel");
 	p.add(new CustomFlexTable(), "FlexTable");
 	p.add(getGrid(), "Grid inside DecoratorPanel");
